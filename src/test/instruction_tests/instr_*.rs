30e6834e447ff6c213a246445835c95322f89bd8
Gregory Comer <me@gregorycomer.com>
2017-10-31 03:27:46 -0600

    Cargo.toml update

--- src/test/instruction_tests/instr_*.rs
+++
@@ -1,8 +0,0 @@
-use ::{BroadcastMode, Instruction, MaskReg, MergeMode, Mnemonic, OperandSize, Reg, RoundingMode};
-use ::RegType::*;
-use ::instruction_def::*;
-use ::Operand::*;
-use ::Reg::*;
-use ::RegScale::*;
-use ::test::run_test;
-
